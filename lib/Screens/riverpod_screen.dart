4862acf46c44fefcc982966a561dbdd9da8f6344
khushi soni <ksoni.param@gmail.com>
2024-07-03 21:13:37 +0530

    StateManagement - RiverPod

---
+++ lib/Screens/riverpod_screen.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:socials/main.dart';
+
+
+//----------------------------- WITH PROVIDER---------------------------
+class RiverpodScreen extends StatefulWidget {
+  const RiverpodScreen({super.key});
+
+  @override
+  State<RiverpodScreen> createState() => _RiverpodScreenState();
+}
+class _RiverpodScreenState extends State<RiverpodScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Consumer(
+          builder: (context,ref,child) {
+            final number = ref.watch(numberProvider);
+            return Text(number.toString());
+          }
+        ),
+      ),
+    );
+  }
+}
+
+
+//-----------------------------WITH CONSUMER WIDGET-------------------------
+// class RiverpodScreen extends ConsumerWidget {
+//   const RiverpodScreen ({super.key});
+
+//   @override
+//   Widget build(BuildContext context, ScopedReader watch) {
+//     final number = watch(numberProvider);
+//     return Scaffold(
+//       body: Center(
+//         child: Text(number.toString()),
+//       ),
+//     );
+//   }
+// }
+
+
+//------------------------------STATE PROVIDER---------------------------------
+// class _RiverpodScreenState extends State<RiverpodScreen> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Center(
+//         child: Consumer(
+//           builder: (context, ref, child) {
+//             // Watch the numberStateProvider to get the current value
+//             final numberState = ref.watch(numberStateProvider);
+//             return Text(numberState.toString(),style: TextStyle(fontSize: 100),);
+//           },
+//         ),
+//       ),
+//       floatingActionButton: Consumer(
+//         builder: (context, ref, child) {
+//           return FloatingActionButton(
+//             onPressed: () {
+//               increment(ref); // Pass ref to the increment function
+//             },
+//             child: Icon(Icons.add), // Add an icon for the button
+//           );
+//         },
+//       ),
+//     );
+//   }
+//   void increment(WidgetRef ref) {
+//     // Use ref.read to access and update the state
+//     ref.read(numberStateProvider.notifier).state++;
+//   }
+// }
--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,57 @@
 import 'package:flutter/material.dart';
-import 'package:socials/Screens/theme_screen.dart';
-import 'package:socials/themes.dart';
-// import 'package:firebase_core/firebase_core.dart';
-// import 'firebase_options.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:socials/Screens/riverpod_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ProviderScope(child: const MyApp()));
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    currentTheme.addListener(() {
-      setState(() {});
-    });
-  }
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const ThemeScreen(),
-      title: 'Flutter Learning Path',
-      theme: CustomTheme.lightTheme,
-      darkTheme: CustomTheme.darkTheme,
-      themeMode: currentTheme.currentTheme,
+      home: RiverpodScreen(),
     );
   }
 }
+
+final numberProvider = Provider<int>((ref) {
+  return 42;
+});
+
+final numberStateProvider = StateProvider<int>((ref) {
+  return 42;
+});
+
+// ---------------------------------ENABLE FOR THEME-------------------------------------
+// class MyApp extends StatefulWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   State<MyApp> createState() => _MyAppState();
+// }
+
+// class _MyAppState extends State<MyApp> {
+//   @override
+//   void initState() {
+//     // TODO: implement initState
+//     super.initState();
+//     currentTheme.addListener(() {
+//       setState(() {});
+//     });
+//   }
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: const ThemeScreen(),
+//       title: 'Flutter Learning Path',
+//       theme: CustomTheme.lightTheme,
+//       darkTheme: CustomTheme.darkTheme,
+//       themeMode: currentTheme.currentTheme,
+//     );
+//   }
+// }
